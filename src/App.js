6c583dc2b84e514c2147dba8ce9dc34c9123be69
dev-ashanaur <devashanaur009@gmail.com>
2022-07-16 13:33:13 +0600

    menu bar design done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
+import {useState} from 'react';
+import styled,{ThemeProvider} from "styled-components";
+import {darkTheme,lightTheme} from './utils/Theme'
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Menu from './components/Menu';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+
+// styled
+const Container = styled.div`
+  display:flex;
+`;
+const Main = styled.div`
+  flex:5;
+  background-color: ${({theme}) => theme.bg};
+`;
+const Wrapper = styled.div`
+  padding: 22px 96px;
+`;
+
 function App() {
+  const [darkMode,setDarkMode] = useState(true);
+
   return (
-    <div>
-      Hello world!
-    </div>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Container>
+        <BrowserRouter>
+          <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Main>
+            <Navbar/>
+            <Wrapper>
+              <Routes>
+                <Route path='/'>
+                  <Route index element={<Home/>} />
+                </Route>
+              </Routes>
+            </Wrapper>
+          </Main>
+        </BrowserRouter>
+      </Container>
+    </ThemeProvider>
   );
 }
 
